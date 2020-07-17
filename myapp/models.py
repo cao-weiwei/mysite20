baa39a2f48628d3f16504f8422a942e1be696020
Weiwei_Cao <weiwei.cao.cn@gmail.com>
2020-07-17 16:12:56 -0400

    Lab7 Part 1

--- myapp/models.py
+++ myapp/models.py
@@ -25,11 +25,18 @@ class Course(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     for_everyone = models.BooleanField(default=True)
     description = models.TextField(max_length=300, null=True, blank=True)
+    # new fields in Lab7
+    interested = models.PositiveIntegerField(default=0)
+    stages = models.PositiveIntegerField(default=3)
 
     def __str__(self):
         return "Topic: {}, Name: {}, Price: {}, For_everyone: {}, Description: {}".format(
             self.topic, self.name, self.price, self.for_everyone, self.description
         )
+
+    def discount(self):
+        """ return discount amount of the price"""
+        return self.price * 0.1
 
 
 class Student(User):
